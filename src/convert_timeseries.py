76650363ddd01c42751d716c65381ae4f2303380
Vlad Gotlib <vladgotlib@gmail.com>
2024-08-30 12:59:22 -0400

    reindexing to 100 at 2014

--- src/convert_timeseries.py
+++ src/convert_timeseries.py
@@ -21,7 +21,7 @@ def load_raw_file(file):
             df = pd.DataFrame(filtered_data)
 
             # Convert 'effectiveDate' from Unix time (milliseconds) to datetime
-            df['effectiveDate'] = pd.to_datetime(df['effectiveDate'], unit='ms')
+            df['effectiveDate'] = pd.to_datetime(df['effectiveDate'], unit='ms').dt.date
 
             # Set 'effectiveDate' as the index
             df.set_index('effectiveDate', inplace=True)
@@ -34,3 +34,22 @@ def load_raw_file(file):
             return df
     except Exception as e:
         print(f"Error processing file {file}: {e}")
+
+# Function to normalize a single column, handling NaN and zero cases
+def normalize_series(series):
+    first_value = series.iloc[0]
+    
+    if pd.isna(first_value) or first_value == 0:
+        # Handling the NaN or zero cases
+        # Option 1: Forward-fill the first value if it's NaN or zero
+        first_value = series.ffill().iloc[0]
+        
+        # Option 2: Replace with a small number to avoid division by zero (if you prefer)
+        # first_value = 0.0001 if first_value == 0 else first_value
+        
+        # If the series is still invalid, return NaN for the entire column
+        if pd.isna(first_value) or first_value == 0:
+            return pd.Series([pd.NA] * len(series), index=series.index)
+    
+    # Normalize the series
+    return (series / first_value) * 100
--- src/load-files.py
+++ src/load-files.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime
 from utils.find_nested_key import find_nested_key_fn
 import os
-from convert_timeseries import load_raw_file
+from convert_timeseries import load_raw_file, normalize_series
 
 def process_json_files_in_folder(directory):
     """
@@ -15,8 +15,10 @@ def process_json_files_in_folder(directory):
         if file_name.endswith('.json'):
             file_path = os.path.join(directory, file_name)
             df = load_raw_file(file_path)
-            if df is not None:
-                data_frames.append(df)
+            df_normalized = df.apply(normalize_series)
+
+            if df_normalized is not None:
+                data_frames.append(df_normalized)
 
     # Concatenate all DataFrames into one, aligned on the 'effectiveDate' index
     if data_frames:
